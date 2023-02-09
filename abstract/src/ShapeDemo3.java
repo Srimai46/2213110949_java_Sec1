0b1fe2e7192988654f17d4571b732fd314e8d7ce
Srimai46 <sira4r2gmail.com>
2023-02-09 11:59:38 +0700

    2

--- abstract/src/ShapeDemo3.java
+++ abstract/src/ShapeDemo3.java
@@ -6,18 +6,22 @@ public class ShapeDemo3 {
 		Scanner sc = new Scanner (System.in);	
 		double[] radius = new double[5];
 		double height[] = new double[5];
-
+		Cylinder[] cylinder = new Cylinder[5];
 		
 		for(int i = 0 ; i<radius.length;i++) {
 			radius[i] = Double.parseDouble(JOptionPane.showInputDialog("Input radius "));			
-			height[i] = Double.parseDouble(JOptionPane.showInputDialog("Input radius "));
+			height[i] = Double.parseDouble(JOptionPane.showInputDialog("Input heigh "));
+			cylinder[i] = new Cylinder(radius[i],height[i]);
+		}
 		
-		}
-		for(int i = 0 ; i<radius.length;i++) {
-			Cylinder cylinder = new Cylinder(radius[i],height[i]);
-			JOptionPane.showMessageDialog(null, "Cylinder "+(i+1)+", volume="+cylinder.getVolume());
 			
-		}
+			JOptionPane.showMessageDialog(null, "Cylinder "+(1)+", volume="+cylinder[0].getVolume()
+					+"/nCylinder "+(2)+", volume="+cylinder[1].getVolume()
+			+"/nCylinder "+(3)+", volume="+cylinder[2].getVolume()
+			+"/nCylinder "+(4)+", volume="+cylinder[3].getVolume()
+			+"/nCylinder "+(5)+", volume="+cylinder[4].getVolume());
+			
+		
 
 	}
 
